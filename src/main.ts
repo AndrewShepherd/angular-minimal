155c577f0fae24cb389579d7c91555fb8458ed01
Andrew Shepherd <shepherd_a@hotmail.com>
2025-04-16 14:58:00 +1000

    Changed to match what the generated application looks like

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
-import {AppModule} from './app/app.module';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
+import { AppComponent } from './components/app/app.component'
 
-// Should look at removing this
-// See https://stackoverflow.com/a/78428511/25216
-platformBrowserDynamic().bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, appConfig)
   .catch(err => console.error(err));
+
