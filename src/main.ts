e2f05957d045ef2e96acf3ce47f8c52dd556e529
Andrew Shepherd <shepherd_a@hotmail.com>
2025-04-16 13:49:11 +1000

    Upgraded to Angular 19

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import {AppModule} from './app/app.module';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
+// Should look at removing this
+// See https://stackoverflow.com/a/78428511/25216
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
